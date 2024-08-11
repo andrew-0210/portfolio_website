b8e417907872a21b9928c4303b40c7370685d73b
Andrew Monthero <andrewmonthero21@gmail.com>
2024-08-11 10:01:09 +0530

    about changes

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -8,14 +8,14 @@ const About = () => {
       <p className='leading-7 text-[1.125rem] tracking-tight text-[#adadad]'>
         I believe in creating delightful user experiences that not only look
         amazing but also function flawlessly. Design is my playground, and code
-        is my secret sauce to bring those designs to life! I'm always hungry to
-        learn and explore new horizons. The possibilities in this space are
-        mind-boggling, and I can't help but dive in headfirst to discover
-        innovative ways to blend technology and user experiences seamlessly.
-        When I'm not diving into design or coding adventures, you'll likely find
-        me lost in the world of music. Whether it's grooving to the beat or
-        getting lost in melodies, music fuels my creative soul and helps me find
-        inspiration in unexpected places.
+        is my secret sauce to bring those designs to life! I&apos;m always
+        hungry to learn and explore new horizons. The possibilities in this
+        space are mind-boggling, and I can&apos;t help but dive in headfirst to
+        discover innovative ways to blend technology and user experiences
+        seamlessly. When I&apos;m not diving into design or coding adventures,
+        you&apos;ll likely find me lost in the world of music. Whether it&apos;s
+        grooving to the beat or getting lost in melodies, music fuels my
+        creative soul and helps me find inspiration in unexpected places.
       </p>
     </div>
   );
