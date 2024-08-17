dd9dfad3f55f8338b37bf7d789d7c1fab095978f
Andrew Monthero <andrewmonthero21@gmail.com>
2024-08-17 11:19:36 +0530

    logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -21,14 +21,7 @@ const Footer = () => {
       <div className='flex justify-between py-[2rem] border-t-[1px] border-[#5c5c5c] text-[#adadad] font-bold tracking-tighter'>
         <span>&copy; All Rights Reserved.</span>
         <Link href='/'>
-          <div className='w-[30]'>
-            <Image
-              src='/andrew.svg'
-              alt='andrew_logo'
-              width={120}
-              height={10}
-            />
-          </div>
+          <Image src='./andrew.svg' alt='andrew_logo' width={120} height={10} />
         </Link>
       </div>
     </section>
