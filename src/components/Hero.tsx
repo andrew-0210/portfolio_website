c9fbefa9d9d5717e9c16568e35ad2d5e47d6b62a
Andrew Monthero <andrewmonthero21@gmail.com>
2024-08-18 18:38:22 +0530

    h1 changed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,9 +7,7 @@ const Hero = () => {
     <div>
       <main className='px-[1rem] h-screen relative overflow-hidden'>
         <section className='flex flex-col justify-center items-center gap-3 md:items-center md:max-w-[90em] md:m-auto h-screen relative z-[99]'>
-          <h1 className='text-5xl font-semibold leading-[3.25rem] tracking-tighter text-center'>
-            I am a Designer + Developer
-          </h1>
+          <h1 className='text-center'>I am a Designer + Developer</h1>
           <p className='text-[#adadad] font-medium text-center tracking-tight text-[1.125rem] leading-7'>
             I believe in creating delightful user experiences that not only look
             amazing but also function flawlessly.
