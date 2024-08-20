65b1920ba077ee5ea17e1acccfe76f5b0a309548
Andrew Monthero <166970322+andrew-0210@users.noreply.github.com>
2024-08-20 19:48:14 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className='flex justify-between py-[2rem] border-t-[1px] border-[#5c5c5c] text-[#adadad] font-semibold tracking-tighter'>
         <span>&copy; All Rights Reserved.</span>
         <Link href='/'>
-          <Image src='./andrew.svg' alt='andrew_logo' width={120} height={10} />
+          <Image src='./andrew.svg' alt='andrew_logo' width={120} height={10} className="dark:invert"/>
         </Link>
       </div>
     </section>
